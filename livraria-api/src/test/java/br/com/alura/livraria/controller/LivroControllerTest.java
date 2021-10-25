f7cc5ab94241508225066d9e270bf5e9e1b80261
digo_ <digo_@rodrigo-desktop>
2021-10-25 00:07:41 -0300

    Teste LivroController

--- livraria-api/src/test/java/br/com/alura/livraria/controller/LivroControllerTest.java
+++ livraria-api/src/test/java/br/com/alura/livraria/controller/LivroControllerTest.java
@@ -1,15 +1,15 @@
 package br.com.alura.livraria.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
 
 import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +18,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import br.com.alura.livraria.model.Autor;
+import br.com.alura.livraria.repositories.AutorRepository;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,8 +28,39 @@ import org.springframework.test.web.servlet.MockMvc;
 @Transactional
 class LivroControllerTest {
 	
+	
 	@Autowired
 	private MockMvc mvc;
+	
+	@Autowired
+	private AutorRepository repository;
+	
+	@Test
+	void deveriaCadastrarLivroComDadosCompletos() throws Exception {
+		Autor autor = new Autor("Rodrigo", "digo@gmail.com", LocalDate.of(1997, 02, 26), "Este é meu mini curriculo");
+		
+		Long id = repository.save(autor).getId();
+		
+		String json = 
+				"{"
+				+ "\"titulo\":\"O jovem programador 4\","
+				+ "\"dataLancamento\":\"2021-09-15\","
+				+ "\"paginas\":125,"
+				+ "\"autor\":{\"id\":"+id+"}"
+				+ "}";
+		
+		
+		mvc
+		.perform(
+				post("/livro")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json))
+				.andExpect(status().isCreated())
+				.andExpect(header().exists("Location"))
+				;
+
+	}
+	
 
 	@Test
 	void naoDeveriaCadastrarLivroComDadosIncompletos() throws Exception {
@@ -39,28 +73,24 @@ class LivroControllerTest {
 				.content(json))
 		.andExpect(status().isBadRequest());
 	}
-	
+
 	@Test
-	void deveriaCadastrarLivroComDadosCompletos() throws Exception {
-		String json = "{\"titulo\":\"O jovem programador 4\","
+	void naoDeveriaCadastrarLivroComAutorInexistente() throws Exception {
+		Long id = 999999l;
+		String json = 
+				"{"
+				+ "\"titulo\":\"O jovem programador 4\","
 				+ "\"dataLancamento\":\"2021-09-15\","
 				+ "\"paginas\":125,"
-				+ "\"autor\":{\"id\":2}}";
+				+ "\"autor\":{\"id\":"+id+"}"
+				+ "}";
 		
 		mvc
 		.perform(
 				post("/livro")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json))
-				.andExpect(status().isCreated())
-				.andExpect(header().exists("Location"))
-				;
-		
-	}
-	
-	@Test
-	void naoDeveriaCadastrarLivroComAutorInexistente() {
-		
+		.andExpect(status().isBadRequest());
 	}
 
 }
